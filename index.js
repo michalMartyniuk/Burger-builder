005151e78dcae7a8352d1407ef0ff28db8255731
michal <michal@localhost.localdomain>
2018-06-14 15:44:40 +0200

    .

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.get('/auth/google/callback', passport.authenticate('google'), (req, res) => 
 })
 
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static('client/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })  
